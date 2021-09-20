98decf11d726a98a25f96af785817a038e22c96d
nihara96 <nbmayurawasala@gmail.com>
2021-09-20 23:18:04 +0530

    Added Model Bottom Sheet

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -22,15 +22,13 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 ElevatedButton(
-                onPressed: (){
-
-                  handleVote(context);
-
-                },
-                child: const Text('Vote'),
-            ),
+                  onPressed: () {
+                    handleVote(context);
+                  },
+                  child: const Text('Vote'),
+                ),
                 ElevatedButton(
-                  onPressed: (){
+                  onPressed: () {
                     registerVoter(context);
                   },
                   child: const Text('Register'),
@@ -38,9 +36,12 @@ class LoginScreen extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () async {
                     String winner = await contractData.getWinner();
-                    Candidate candidate = Provider.of<ContractData>(context,listen: false).getCandidate(int.parse(winner));
-                    showWinner(context,
-                        candidate.name,
+                    Candidate candidate =
+                        Provider.of<ContractData>(context, listen: false)
+                            .getCandidate(int.parse(winner));
+                    showWinner(
+                      context,
+                      candidate.name,
                       candidate.imageUrl,
                     );
                   },
@@ -54,190 +55,230 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  void showWinner(context,winner,image) {
-
+  void showWinner(context, winner, image) {
     showDialog(
-        context: context,
-        builder: (_)
-        {
-          return AlertDialog(
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Winner Is $winner',
-                style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),
-                ),
-                 const SizedBox(height: 20.0,),
-                 CircleAvatar(
-                  maxRadius: 60.0,
-                  backgroundImage: NetworkImage(image),
-                ),
-                const SizedBox(height: 20.0,),
-                ElevatedButton(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancel'),
-                ),
-              ],
-            ),
-          );
-        },
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Winner Is $winner',
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 25.0),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              CircleAvatar(
+                maxRadius: 60.0,
+                backgroundImage: NetworkImage(image),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
   void handleVote(context) {
-
     // var contractLink = Provider.of<ContractData>(context,listen: false);
     TextEditingController voterAddress = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
 
-    showDialog(context: context,
-        builder: (_)
-        {
-          return AlertDialog(
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Voting',style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
-                Padding(padding: const EdgeInsets.only(top: 10,bottom: 8.0),
-                child: TextField(
-                  controller: voterAddress,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext context) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Container(
+                color: const Color(0xFF737373),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0))),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20.0, horizontal: 10.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Enter Wallet Address'),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(fontSize: 17),
+                              hintText: 'Address',
+                              contentPadding: EdgeInsets.all(20),
+                            ),
+                            controller: voterAddress,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Center(
+                            child: SizedBox(
+                              width: 150.0,
+                              child: ElevatedButton(
+                                child: const Text("OK"),
+                                onPressed: () async {
+                                  var contractLink = Provider.of<ContractData>(
+                                      context,
+                                      listen: false);
+
+                                  try {
+                                    await contractLink
+                                        .checkVoted(voterAddress.text);
+                                    Navigator.pop(context);
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => VoteScreen(
+                                                address: voterAddress.text,
+                                              )),
+                                    );
+                                  } catch (e) {
+                                    Navigator.pop(context);
+
+                                    RegExp voted = RegExp(
+                                        "\\b" + "Already voted" + "\\b",
+                                        caseSensitive: false);
+                                    RegExp noRights = RegExp(
+                                        "\\b" + "Has no right to vote" + "\\b",
+                                        caseSensitive: false);
+
+                                    if (voted.hasMatch(e.toString())) {
+                                      Fluttertoast.showToast(
+                                        msg: "You are already voted",
+                                      );
+                                    } else if (noRights
+                                        .hasMatch(e.toString())) {
+                                      Fluttertoast.showToast(
+                                        msg: "Has no right to vote",
+                                      );
+                                    } else {
+                                      Fluttertoast.showToast(
+                                        msg: "You cant vote this time",
+                                      );
+                                    }
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    labelText: "Voter Address",
-                    hintText: "Enter Address"
                   ),
                 ),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () async{
-
-                          var contractLink = Provider.of<ContractData>(context, listen: false);
-
-                          try{
-                            await contractLink.checkVoted(voterAddress.text);
-                            Navigator.pop(context);
-                            Navigator.push(context,
-                                MaterialPageRoute(builder:(context)=> VoteScreen(address: voterAddress.text,)),
-                            );
-                          }catch(e){
-                            Navigator.pop(context);
-
-                            RegExp voted = RegExp( "\\b" + "Already voted" + "\\b", caseSensitive: false);
-                            RegExp noRights = RegExp( "\\b" + "Has no right to vote" + "\\b", caseSensitive: false);
-
-                            if(voted.hasMatch(e.toString()))
-                            {
-                              Fluttertoast.showToast(
-                                msg: "You are already voted",
-                              );
-                            }else if(noRights.hasMatch(e.toString())){
-                              Fluttertoast.showToast(
-                                msg: "Has no right to vote",
-                              );
-                            }
-                            else{
-                              Fluttertoast.showToast(
-                                msg: "You cant vote this time",
-                              );
-                            }
-
-                          }
-
-                        },
-                        child: const Text('Ok'),
-                    )
-                  ],
-                ),
-              ],
+              ),
             ),
           );
-        }
-    );
-
+        });
   }
 
   void registerVoter(context) {
     var contractLink = Provider.of<ContractData>(context, listen: false);
     TextEditingController voterAddress = TextEditingController();
     TextEditingController adminAddress = TextEditingController();
-    showDialog(
+    final _formKey = GlobalKey<FormState>();
+
+    showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
-        builder: (_) {
-          return AlertDialog(
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-               const Text(
-                  "Register Voter",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                Padding(
-                  padding:const EdgeInsets.only(top: 18.0, bottom: 8.0),
-                  child: TextField(
-                    controller: adminAddress,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "Admin Address",
-                        hintText: "Enter Admin Address..."),
+        builder: (BuildContext context) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Container(
+                color: const Color(0xFF737373),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0))),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20.0, horizontal: 10.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Enter Admin Wallet Address'),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(fontSize: 17),
+                              hintText: 'Admin\'s Address',
+                              contentPadding: EdgeInsets.all(20),
+                            ),
+                            controller: adminAddress,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          const Text('Enter Voter Wallet Address'),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(fontSize: 17),
+                              hintText: 'Voter\'s Address',
+                              contentPadding: EdgeInsets.all(20),
+                            ),
+                            controller: voterAddress,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Center(
+                            child: SizedBox(
+                              width: 150.0,
+                              child: ElevatedButton(
+                                child: const Text("OK"),
+                                onPressed: () async {
+                                  try {
+                                    contractLink.registerVoter(
+                                        voterAddress.text, adminAddress.text);
+                                    Fluttertoast.showToast(
+                                      msg:
+                                          "Voter ${voterAddress.text.substring(0, 5)}XXX Registered.",
+                                    );
+                                  } catch (e) {
+                                    Fluttertoast.showToast(
+                                      msg: 'Registration Error !',
+                                    );
+                                  }
+
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding:const EdgeInsets.only(bottom: 8.0),
-                  child: TextField(
-                    controller: voterAddress,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "Voter Address",
-                        hintText: "Enter Voter Address..."),
-                  ),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child:const Text("Cancel")),
-                    Padding(
-                      padding:const EdgeInsets.only(left: 8.0),
-                      child: ElevatedButton(
-                          onPressed: () {
-
-                            try{
-                              contractLink.registerVoter(
-                                  voterAddress.text, adminAddress.text);
-                              Fluttertoast.showToast(
-                                msg: "Voter ${voterAddress.text.substring(0, 5)}XXX Registered.",
-                              );
-
-                            }catch(e){
-                              Fluttertoast.showToast(
-                                msg:'Registration Error !',
-                              );
-                            }
-
-
-                            Navigator.pop(context);
-                          },
-                          child:const Text("Register")),
-                    )
-                  ],
-                )
-              ],
+              ),
             ),
           );
         });
   }
-
 }
