70ee6ab92cb280fc46e285439a8f134d21b880ef
nihara96 <nbmayurawasala@gmail.com>
2021-11-15 20:02:24 -0800

    winner screen added

---
+++ lib/winner_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WinnerScreen extends StatelessWidget {
+  const WinnerScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Winner Screen'),
+      ),
+    );
+  }
+}
